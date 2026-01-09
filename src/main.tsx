4fc25c5ab013f74f935053766b8673c371c596e0
jamesbond008 <33677628+jamesbond008@users.noreply.github.com>
2026-01-09 15:06:08 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // 只要 App.tsx 在同级目录下，这样写就能找到
-import './index.css'
+import App from './App' // ✅ 只要你把 App.tsx 移进了 src，这行就能找到文件
+import './index.css'   // ✅ 这行对应你 src 里的 index.css
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

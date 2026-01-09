201d6fc22e369e98f598efbabd3a6c650770ce12
jamesbond008 <33677628+jamesbond008@users.noreply.github.com>
2026-01-09 15:09:26 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // ✅ 只要你把 App.tsx 移进了 src，这行就能找到文件
-import './index.css'   // ✅ 这行对应你 src 里的 index.css
+import App from './App.tsx' // ✅ 加上 .tsx 后缀，强制 Vite 查找精确文件
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

1b4a30ec1ea22c3580c11f6a0e3e9a22e1555066
jamesbond008 <33677628+jamesbond008@users.noreply.github.com>
2026-01-09 15:05:05 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css' // ✅ 关键：在这里引入 CSS，Tailwind 才会生效
+import App from './App' // 只要 App.tsx 在同级目录下，这样写就能找到
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
